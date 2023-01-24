fa66708b31bacb3f718d4fadfb4e7b8ac8fbfb41
NaturalCode4 <naturalcode4@gmail.com>
2023-01-23 22:39:25 -0700

    boxes, optimistic load, spinner

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+
+///// mock api
+const apiColors = ['olive', 'wheat', 'tomato', 'pink', 'burlywood', 'aquamarine', 'tan', 'white', 'brown', 'beige']
+
+const getColorsIndex = (colorsArray) => { //function that gets index within array
+  return Math.floor(Math.random()*colorsArray.length)
+}
+
+const getColor = () => {
+  const colorPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(apiColors[getColorsIndex(apiColors)])
+        reject('there was an error') // will never reach
+      }, 3000)
+  })
+  return colorPromise
+}
 
 function App() {
+
+  const [blueBoxes, setBlueBoxes] = useState(['none'])
+
+  const addBox = async () => {
+    // if you have a promise rejection that's not being caught (catch or .catch), app will crash
+    try {
+      let color = 'none'
+      setBlueBoxes([...blueBoxes, `${color}`]) // React stale closure hooks
+      const insertionIndex = blueBoxes.length
+      color = await getColor()
+      setBlueBoxes(prev => {
+        const arr = [...prev]
+        arr.splice(insertionIndex, 1, `${color}`)
+        return arr
+      })
+    }
+    catch (errMessage){
+      alert(errMessage) // make alert here
+    }
+  }
+
+  const removeBox = () => {
+    setBlueBoxes([...blueBoxes.slice(0, blueBoxes.length-1)])
+  }
+
+  // API delivers a color (already randomized) from ['red', 'orange', 'yellow', 'green', 'blue']
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Colored Boxes</h1>
+        <div>
+          {blueBoxes.map((color, index) => {
+            // async function returning a promise that we need to await before rendering color
+            if ((index + 1) % 2 === 0) {
+              return <div className="box" onClick={removeBox} style={{backgroundColor: color}}>
+                {color === 'none' && <img src={logo} className="App-logo" alt="logo" />}
+              </div>
+            } else {
+              return <div className="box" onClick={addBox} style={{backgroundColor: color}}>
+                {color === 'none' && <img src={logo} className="App-logo" alt="logo" />}
+              </div>
+            }
+            })
+          }
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
+// every even numbered box needs to be able to delete itself
+// meanwhile odd numbered will add
+
+// randomly generate color on box from 5 that simulates an api. I want it to return a function with a 3 second delay before getting new box
+// every time you make a new box, you need to know what the color is
+// freedom to choose format api is delivering in
---
+++ src/Gustaf.js
@@ -0,0 +1,8 @@
+/*
+Give all posts that have card
+Search bar ==> live filter posts by content words. Searches for in title or postBody. Content has that string in it
+Search bar also (could be seperate or same) look for names ==> all that users posts, preferably on a card
+
+[  le    ] gives all posts from Leanne and also from Clementine
+
+*/
